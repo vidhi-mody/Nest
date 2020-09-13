720fbd4e5fd2c7e856207fb318573ab297b26087
vidhi-mody <vidhimody6@gmail.com>
2020-09-14 00:49:42 +0530

    add: dto and service

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all items';
+  }
+
+  @Get()
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  @Post()
+  create(@Body() CreateItemDto: CreateItemDto): string {
+    return 'Create Item';
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
   }
 }

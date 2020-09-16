fdd8d95fc6e66dff474f68ddbb2ec1cdb810fbec
vidhi-mody <vidhimody6@gmail.com>
2020-09-17 01:16:46 +0530

    add: mongodb

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongodbURI:
+    'YOUR URL',
+};
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { ItemSchema } from './schemas/item.schema';
 
 @Module({
-  imports: [],
-  controllers: [ ItemsController],
-  providers: [ ItemsService],
+  imports: [MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }])],
+  controllers: [ItemsController],
+  providers: [ItemsService],
 })
 export class ItemsModule {}
